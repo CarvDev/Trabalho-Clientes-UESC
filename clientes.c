a0d1d0060e30b7cbf9cdd740255b600b74a64609
CarvDev <arthurcarvlima@outlook.com>
2025-11-19 15:21:16 -0300

    Adicionado módulo de clientes

--- clientes.c
+++ clientes.c
@@ -1,10 +1,35 @@
+#include "clientes.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "auxiliar.h"
+#include "menus.h"
 
-typedef struct incluir_clientes
-{
-    
-    int codigo;
-    char clientes[20];
-
-} incluir_clientes;
+void executar_modulo_clientes() {
+    int opcao;
+    do {
+        limpar_tela(0);
+        imprimir_menu_clientes();
+        opcao = obter_opcao();
+        switch (opcao)
+        {
+            case 1:
+                // Incluir Cliente
+                break;
+            case 2:
+                // Excluir Cliente
+                break;
+            case 3:
+                // Alterar Cliente
+                break;
+            case 4:
+                // Mostrar Cadastro de Clientes
+                break;
+            case 5:
+                // Retornar
+                break;
+            default:
+                printf("[Opção inválida]\nTente novamente...\n\n");
+                break;
+        }
+    } while (opcao != 5);
+}
---
+++ clientes.h
@@ -0,0 +1,14 @@
+#ifndef CLIENTES_H
+#define CLIENTES_H
+
+void executar_modulo_clientes(void);
+/* 
+Esse módulo inicia um do-while, que limpa a tela, imprime o menu de clientes, 
+solicita que o usuário selecione a opção desejada, e então armazena o input
+numa variável local. Essa variável será passada para um switch(case), que executará
+a função correspondente a cada opção disponível no módulo. Caso o usuário digite 5
+o programa retornará ao menu principal, quebrando o loop. Caso digite um número
+inválido o programa irá informar.
+*/
+
+#endif
