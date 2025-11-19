18d69476207f6419ffaa1d4f1737e6f324f63485
CarvDev <arthurcarvlima@outlook.com>
2025-11-19 15:20:58 -0300

    Movida função obter_opcao() para auxiliar.c/h

--- auxiliar.c
+++ auxiliar.c
@@ -17,3 +17,26 @@ void limpar_tela(int op) {
         system("clear");
     #endif
 }
+
+int obter_opcao() {
+    int opc; // Variável para armazer a opção selecionada
+    int scanf_retorno; // Variável para checar o retorno do scanf
+
+    while (1) {
+        printf("> "); // caractere '>' para uma melhor exibição do programa no terminal
+        
+        // Armazena o valor de retorno da função scanf
+        scanf_retorno = scanf("%d", &opc);
+
+        // Caso de erro para quando o input NÃO for um número (ex: 'a') (scanf_retorno será 0)
+        if (scanf_retorno == 0) {
+            printf("[Erro: Digite apenas números]\nTente novamente...\n\n");
+            limpar_buffer(); // Limpa o input inválido (ex: 'a\n')
+            continue; // Pula para a próxima iteração do loop
+
+        } else break; // Quebra o loop
+    }
+    
+    limpar_buffer();
+    return opc; // Retorna a opção selecionada
+}
--- auxiliar.h
+++ auxiliar.h
@@ -1,5 +1,17 @@
+#ifndef AUXILIAR_H
+#define AUXILIAR_H
+
 // função para limpar o buffer, mais eficaz e portável que fflush, que só funciona no Windows
 void limpar_buffer(void);
 
 //limpa tela
 void limpar_tela(int op);
+
+/*
+Imprime o caractere '>', em seguida solicita para o usuário o input de um 
+número inteiro e o retorna. Possui uma verificação para caso o usuário digite 
+uma letra, evitando um loop infinito (devido à poluição do buffer)
+*/
+int obter_opcao(void);
+
+#endif
