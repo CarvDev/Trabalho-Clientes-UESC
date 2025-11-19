49f48ec73d29cd8edd0c8a9fb4af74f7a22095c6
CarvDev <arthurcarvlima@outlook.com>
2025-11-19 15:24:13 -0300

    Criado módulo de vendas

---
+++ vendas.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "auxiliar.h"
+#include "vendas.h"
+
+// falta implementar
+void executar_modulo_vendas() {
+    limpar_tela(0);
+    //efetuar_venda();
+}
---
+++ vendas.h
@@ -0,0 +1,6 @@
+#ifndef VENDAS_H
+#define VENDAS_H
+
+void executar_modulo_vendas();
+
+#endif
